2912630d63770d39ff47c11308d3bfb0c8de12b6
seoyoungryu <komnaru99@naver.com>
2023-05-25 17:43:12 +0900

    2023-05-25

---
+++ node_proj/ex-db/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const oracledb = require('oracledb');
+const app = express();
+
+app.set('view engine', 'ejs');
+//oracle 정보
+const dbConfig = {
+    user : 'java'
+    ,password : 'oracle'
+    ,connectString : '127.0.0.1:1521/XE'
+
+}
+app.get('/',async(req,res)=>{
+    let conn;
+    try{
+        conn = await oracledb.getConnection(dbConfig);
+        const result = await conn.execute(`SELECT * FROM employees`);
+        res.render('index', {data:result.rows});
+    }catch(err){
+        console.log(err);
+        res.status(500).send('error');
+    }finally{
+        if(conn){
+            try{
+                await conn.close();
+            }catch(e){
+                console.log(e);
+            }
+        }
+    }
+});
+app.listen(3000,()=>{
+    console.log('start server');
+});
