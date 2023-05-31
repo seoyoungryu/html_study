8a91879963974516719401bf18abf6a50eb082c9
seoyoungryu <komnaru99@naver.com>
2023-05-31 17:38:41 +0900

    2023-05-31

---
+++ search_medicine/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+/* //cors 오류 발생 시
+const cors =require('cors');
+//전체 허용
+app.use(cors()); */
+app.set('view engine', 'ejs');
+//json 형태로 request 데이터를 받을 수 있음
+app.use(express.json());
+app.get('/', async(req,res)=> {
+    res.render('index');
+});
+
+app.get('/test',async(req, res)=>{
+    console.log('요청:', req.query);
+    console.log('요청:', req.query.nm);
+    res.send({flag : 'Y'});
+});
+app.post('/test', async(req,res)=>{
+    console.log(req.body);
+    res.send({flag : 'Y'});
+});
+app.listen(3000, ()=>{
+    console.log('start');
+})
